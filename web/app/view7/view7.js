93a848b754698f900fd137589fb55de0aa43bca4
biggiejr <jrmacej@gmail.com>
2015-11-24 15:22:24 +0100

    Minor changes

--- web/app/view7/view7.js
+++ web/app/view7/view7.js
@@ -5,23 +5,12 @@ angular.module('myApp.view7', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view7', {
     templateUrl: 'app/view7/view7.html',
-    controller: 'View7Ctrl'
+    controller: 'View7Ctrl',
+    controllerAs : 'ctrl'
   });
 }])
-        
-        
-       .controller('View7Ctrl', function ($http, $scope) {
-    var person = '{"username" : "","password" : "","role" : "user"}';
-   $http({
-        type: 'POST',
-        url: "api/demouser",
-        contentType: 'application/json',
-        data: person,
-        success: function (data) {
-            refreshTable();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert(textStatus + " on POST");
-        }
-    });
-});
+
+.controller('View7Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
+  this.msgFromFactory = InfoFactory.getInfo();
+  this.msgFromService = InfoService.getInfo();
+}]);
